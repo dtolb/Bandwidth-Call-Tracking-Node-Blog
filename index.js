c572cfdc88db3392d112d834332874f03ed1e5b6
Daniel Tolbert <dtolbert@bandwidth.com>
2015-10-26 16:53:06 -0400

    Now checks for application or creates one and orders number

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var Promise = require('bluebird');
 var app = express();
+var config = require('./bandwidth.json');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var xml = require('node-bandwidth').xml;
-var tn = "+14154292562";
+var tn;
+var bandwidth = require("node-bandwidth");
+var xml = bandwidth.xml;
+var Application = Promise.promisifyAll(bandwidth.Application);
+var PhoneNumber = Promise.promisifyAll(bandwidth.PhoneNumber);
+var AvailableNumber = Promise.promisifyAll(bandwidth.AvailableNumber);
+
+var appName = "Bandwidth-Call-Tracking-Blog";
+var client = new bandwidth.Client(
+	config.userId,
+	config.apiToken,
+	config.apiSecret);
 var cdr = {
 	numCalls: 0
 };
@@ -17,6 +29,69 @@ response.push(speakSentence);
 // Create the xml to send
 var bxml = response.toXml();
 
+//Checks the current Applications to see if we have one.
+var configureApplication = function () {
+	return Application.listAsync(client, {
+		size: 1000
+	})
+	.then(function (applications) {
+		var applicationId = searchForApplication(applications, appName);
+		if(applicationId !== false) {
+			return fetchTNByAppId(applicationId);
+		}
+		else {
+			return newApplication();
+		}
+	});
+};
+
+// Searches through application names and returns ID if matched
+var searchForApplication = function (applications, name) {
+	for (var i = 0; i < applications.length; i++) {
+			if ( applications[i].name === name) {
+				return applications[i].id;
+			}
+		}
+	return false;
+};
+
+// Gets the first number associated with an application
+var fetchTNByAppId = function (applicationId) {
+	return PhoneNumber.listAsync(client, {
+		applicationId: applicationId
+	})
+	.then(function (numbers) {
+		tn = numbers[0].number;
+	});
+};
+
+// Creates a new application then orders a number and assigns it to application
+var newApplication =function () {
+	var applicationId;
+	return Application.createAsync(client, {
+			name: appName,
+			incomingCallUrl: config.baseUrl + "/incomingCall/",
+			callbackHttpMethod: "get",
+			autoAnswer: true
+		})
+		.then(function(application) {
+			//search an available number
+			applicationId = application.id;
+			return AvailableNumber.searchLocalAsync(client, {
+				areaCode: "415",
+				quantity: 1
+			});
+		})
+		.then(function(numbers) {
+			// and reserve it
+			tn = numbers[0].number;
+			return PhoneNumber.createAsync(client, {
+				number: tn,
+				applicationId: applicationId
+			});
+		});
+};
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('static'));
 app.get('/', function(req, res){
@@ -27,7 +102,7 @@ app.get('/', function(req, res){
 app.get('/incomingCall', function(req, res) {
 	if(req.query && req.query.eventType && req.query.eventType === 'answer') {
 		if(req.query.to === tn) {
-			cdr.tn1.numCalls += 1;
+			cdr.numCalls += 1;
 			io.emit('numCalls1', cdr.numCalls);
 			cdr.callStart = new Date();
 			io.emit('caller1', req.query.from);
@@ -48,12 +123,18 @@ app.get('/incomingCall', function(req, res) {
 	}
 });
 
-io.on('connection', function(socket){
-	socket.emit('connected', 'Connected!');
-	io.emit('numCalls1', cdr.numCalls);
+configureApplication()
+.then(function () {
+	io.on('connection', function(socket){
+		socket.emit('connected', 'Connected!');
+		io.emit('numCalls1', cdr.numCalls);
+		io.emit('tn', tn);
+	});
 
+	http.listen(app.get('port'), function(){
+		console.log('listening on *:' + app.get('port'));
+	});
 });
 
-http.listen(app.get('port'), function(){
-	console.log('listening on *:' + app.get('port'));
-});
+
+
